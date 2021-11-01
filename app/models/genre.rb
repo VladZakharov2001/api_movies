fbe1d7db93c6c6bbe89c0734be82710dcb8891ad
kodep <vladislav.zaharov@kodep.ru>
2021-11-01 14:14:22 +0300

    delete space

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,4 @@ class Genre < ApplicationRecord
   has_many :user_genres, dependent: :destroy
   has_many :users, through: :user_genres, dependent: :destroy
   validates :external_genre_id, presence: true, numericality: { only_integer: true }, uniqueness: true
-
-  
- 
 end
