61864368e500f512505fa8f3f4786a3812982b0d
kodep <vladislav.zaharov@kodep.ru>
2021-11-03 10:43:58 +0300

    add congig logger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,15 +11,19 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info").downcase.strip.to_sym
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -50,7 +54,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.encoding =  'UTF-8'
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
